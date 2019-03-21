77445b9a82c1de59064486d0d7afe1fa44ea362f
kaiser <765934690@qq.com>
2019-03-21 20:08:07 +0800

    add comment

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ describe('this', function() {
       say: function() {
         setTimeout(() => {
           // this 是什么？想想为什么？
+          // this 是 say 的 this, 而 say 的 this 通过隐式绑定规则绑定,其值为 obj
           this.should.equal(obj)
           done()
         }, 0)
@@ -15,6 +16,7 @@ describe('this', function() {
   it('global', function() {
     function test() {
       // this 是什么？想想为什么？
+      // 默认绑定
       this.should.equal(global)
     }
     test()
@@ -28,6 +30,7 @@ describe('this', function() {
             // this 是什么？想想为什么？
             this.should.equal(global)
           }
+          // 硬绑定 但此时 obj 虽已声明但未赋值 故为 undefined 故 this 为 global
           return _say.bind(obj)
         })()
       }
@@ -41,6 +44,7 @@ describe('this', function() {
           // this 是什么？想想为什么？
           this.should.equal(obj)
         }
+        // 硬绑定 但此时 obj = {} 故 this 为 obj
         return _say.bind(obj)
       })()
       obj.say()
